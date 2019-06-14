f10b4c43eafd151a304848df067e8b7053426655
panchalneha <panchal.n@husky.neu.edu>
2019-06-14 09:46:25 -0400

    Update Unit test

--- webapp/src/main/java/com/neu/csye6225/webApplication/entity/Books.java
+++ webapp/src/main/java/com/neu/csye6225/webApplication/entity/Books.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.Size;
 public class Books {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private String id;
 	
--- webapp/src/test/java/com/neu/csye6225/webApplication/AppTest.java
+++ webapp/src/test/java/com/neu/csye6225/webApplication/AppTest.java
@@ -3,36 +3,47 @@ package com.neu.csye6225.webApplication;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.springframework.stereotype.Service;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	
+	@Service
+	public class ProductService {
+	   public String getBookName() {
+	      return "Book1";
+	   } 
+	   
+	}
+	
 }
+	
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
+//
+//    /**
+//     * Rigourous Test :-)
+//     */
+//    public void testApp()
+//    {
+//        assertTrue( true );
+//    }
+//}
