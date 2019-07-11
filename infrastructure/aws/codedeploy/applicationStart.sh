b527582961137af010f31429af947dd1c1598b4b
bhavyaharidas <bhavya.haridas93@gmail.com>
2019-07-10 23:28:30 -0400

    updating app start script

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo systemctl start tomcat.service
+#sudo systemctl start tomcat.service
 cd /opt/tomcat/webapps
 java -jar webApplication-0.0.1-SNAPSHOT.jar \
 --regionName=us-east-1 --bucketName=csye6225-su19-haridasb-me-csye6225-com \
