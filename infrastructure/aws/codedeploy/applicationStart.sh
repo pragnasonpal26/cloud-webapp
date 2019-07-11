b184a695b6888b7a8d07a7ffda562ce5843abbee
bhavyaharidas <bhavya.haridas93@gmail.com>
2019-07-10 23:22:09 -0400

    updating app start script

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 sudo systemctl start tomcat.service
+cd /opt/tomcat/webapps
+java -jar webApplication-0.0.1-SNAPSHOT.jar \
+--regionName=us-east-1 --bucketName=csye6225-su19-haridasb-me-csye6225-com \
+--spring.profile.active=prod \
+--spring.datasource.url=jdbc:mysql://csye6225-su19.cys6ezwflabj.us-east-1.rds.amazonaws.com:3306/csye6225?serverTimezone=UTC&createDatabaseIfNotExist=true
