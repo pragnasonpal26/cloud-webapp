4e6792ab1be5c0f6ad7575ead546c111d00a0da4
bhavyaharidas <bhavya.haridas93@gmail.com>
2019-07-11 21:48:46 -0400

    prop file update

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -5,4 +5,5 @@ cd /opt/tomcat/webapps
 java -jar webApplication-0.0.1-SNAPSHOT.jar \
 --regionName=us-east-1 --bucketName=csye6225-su19-haridasb-me-csye6225-com \
 --spring.profile.active=prod \
---spring.datasource.url=jdbc:mysql://csye6225-su19.cys6ezwflabj.us-east-1.rds.amazonaws.com:3306/csye6225?serverTimezone=UTC&createDatabaseIfNotExist=true
+--spring.datasource.url=jdbc:mysql://csye6225-su19.cys6ezwflabj.us-east-1.rds.amazonaws.com:3306/csye6225?serverTimezone=UTC&createDatabaseIfNotExist=true \
+--spring.datasource.username=csye6225master --spring.datasource.password=csye6225password
