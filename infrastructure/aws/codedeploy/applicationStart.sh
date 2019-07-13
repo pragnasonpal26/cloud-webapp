adcde125b60f44b3afb3505453fc1385307cce64
bhavyaharidas <bhavya.haridas93@gmail.com>
2019-07-12 23:35:27 -0400

    dev null added

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -4,4 +4,4 @@
 cd /opt/tomcat/webapps
 userdata=$(curl http://169.254.169.254/latest/user-data)
 
-java -jar webApplication-0.0.1-SNAPSHOT.jar $userdata
+java -jar webApplication-0.0.1-SNAPSHOT.jar $userdata > /dev/null 2> /dev/null < /dev/null &
