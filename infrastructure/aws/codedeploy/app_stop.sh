50c72b3538520f8982965d18953d5b04bab5f453
bhavyaharidas <bhavya.haridas93@gmail.com>
2019-07-17 22:15:58 -0400

    auto deploy

--- infrastructure/aws/codedeploy/app_stop.sh
+++ infrastructure/aws/codedeploy/app_stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo systemctl stop tomcat.service
+
+sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/webApplication-0.0.1-SNAPSHOT.jar
