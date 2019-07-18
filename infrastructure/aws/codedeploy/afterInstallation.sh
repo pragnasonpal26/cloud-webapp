b3c8cefdd857a957169e24f9afb15f1f9769733d
bhavyaharidas <bhavya.haridas93@gmail.com>
2019-07-17 22:11:07 -0400

    auto deploy

--- infrastructure/aws/codedeploy/afterInstallation.sh
+++ infrastructure/aws/codedeploy/afterInstallation.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-sudo systemctl stop tomcat.service
+#sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/webApplication-0.0.1-SNAPSHOT.jar
 
-sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/webApplication-0.0.1-SNAPSHOT.jar
-
-sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
+sudo chown tomcat:tomcat /opt/tomcat/webapps/webApplication-0.0.1-SNAPSHOT.jar
 
 # cleanup log files
 sudo rm -rf /opt/tomcat/logs/catalina*
