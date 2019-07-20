9351226c7532eafa937b2e1ce02373f17daaacd8
bhavyaharidas <bhavya.haridas93@gmail.com>
2019-07-20 12:21:01 -0400

    test

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -5,4 +5,4 @@ sudo systemctl start tomcat.service
 #cd /opt/tomcat/webapps
 #userdata=$(curl http://169.254.169.254/latest/user-data)
 
-#java -jar webApplication-0.0.1-SNAPSHOT.jar $userdata > /dev/null 2> /dev/null < /dev/null &
+java -jar webApplication-0.0.1-SNAPSHOT.jar $userdata >> /etc/default/tomcat
