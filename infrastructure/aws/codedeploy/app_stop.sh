db26dd50a5eadbf643b9c0aafa428b7459f532f8
bhavyaharidas <bhavya.haridas93@gmail.com>
2019-07-21 09:32:33 -0400

    Temporarily Adding cloudwatch json and service files

--- infrastructure/aws/codedeploy/app_stop.sh
+++ infrastructure/aws/codedeploy/app_stop.sh
@@ -2,4 +2,4 @@
 
 sudo systemctl stop tomcat.service
 
-sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/webApplication-0.0.1-SNAPSHOT.jar
+#sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/webApplication-0.0.1-SNAPSHOT.jar
