d26f4f76a0c61bdcadee414c27c16a35adb76a21
panchalneha <43355497+panchalneha@users.noreply.github.com>
2019-07-26 18:29:57 -0400

    Update BooksController.java

--- webapp/src/main/java/com/neu/csye6225/webApplication/controllers/BooksController.java
+++ webapp/src/main/java/com/neu/csye6225/webApplication/controllers/BooksController.java
@@ -78,43 +78,4 @@ public class BooksController {
 		return new ResponseEntity<>(postBook, HttpStatus.NO_CONTENT);
 	}
 
-	@GetMapping("/book/{idBook}/image/{idImage}")
-	@ResponseStatus(HttpStatus.OK)
-	public String getCoverImage(@PathVariable String idBook, @PathVariable String idImage) {
-		Optional<Books> singleBook = booksService.getBooks(UUID.fromString(idBook));
-		Images image = singleBook.get().getImage();
-		return imagesService.getImageUrl(image);
-	}
-
-	@PostMapping("/book/{idBook}/image")
-	@ResponseStatus(HttpStatus.OK)
-	public Images postImage(@RequestParam("file") MultipartFile file, @PathVariable String idBook) {
-		Books singleBook = booksService.getBooks(UUID.fromString(idBook)).get();
-		Images image = new Images();
-		image.setUrl(imagesService.upload(file,image));
-		singleBook.setImage(image);
-		booksService.update(singleBook);
-		return image;
-	}
-
-	@PutMapping("/book/{idBook}/image/{idImage}")
-	public Images updateImage(@PathVariable String idBook, @PathVariable String idImage, @RequestParam("file") MultipartFile file) {
-		Books singleBook = booksService.getBooks(UUID.fromString(idBook)).get();
-		Images image = singleBook.getImage();
-		image.setUrl(imagesService.updateImage(file,image));
-		imagesService.update(image);
-		return image;
-	}
-
-	@DeleteMapping("/book/{idBook}/image/{idImage}")
-	public ResponseEntity<String> deleteImage(@PathVariable String idImage, @PathVariable String idBook) {
-		Books singleBook = booksService.getBooks(UUID.fromString(idBook)).get();
-		Images image = singleBook.getImage();
-		imagesService.delete(image);
-		singleBook.setImage(null);
-		booksService.update(singleBook);
-		imagesService.deleteImages(UUID.fromString(idImage));
-		return new ResponseEntity("Deleted successfully!", HttpStatus.OK);
-	}
-
 }
