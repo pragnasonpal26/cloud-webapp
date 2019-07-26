dffd9d6dc3cb8628140ffbdd51f1408449131e57
bhavyaharidas <bhavya.haridas93@gmail.com>
2019-07-25 22:27:25 -0400

    app stop

--- infrastructure/aws/codedeploy/app_stop.sh
+++ infrastructure/aws/codedeploy/app_stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-if apid=$(pgrep -f webApplication-0.0.1-SNAPSHOT.jar)
-then
-    ps -ef | grep webApplication-0.0.1-SNAPSHOT | grep -v grep | awk '{print $2}' | xargs kill
-fi
+#if apid=$(pgrep -f webApplication-0.0.1-SNAPSHOT.jar)
+#then
+    #ps -ef | grep webApplication-0.0.1-SNAPSHOT | grep -v grep | awk '{print $2}' | xargs kill
+#fi
 
 #sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/webApplication-0.0.1-SNAPSHOT.jar
