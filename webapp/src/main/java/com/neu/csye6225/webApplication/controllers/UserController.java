b30e651375480f09c458117f8004b9512095b50e
bhavyaharidas <bhavya.haridas93@gmail.com>
2019-07-30 23:44:47 -0400

    added reset end point to send message to SNS

--- webapp/src/main/java/com/neu/csye6225/webApplication/controllers/UserController.java
+++ webapp/src/main/java/com/neu/csye6225/webApplication/controllers/UserController.java
@@ -8,19 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.neu.csye6225.webApplication.entity.User;
 import com.neu.csye6225.webApplication.service.UserService;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/user")
 public class UserController {
 
 	private final static Logger logger = LoggerFactory.getLogger(BooksController.class);
@@ -53,6 +47,15 @@ public class UserController {
 			logger.error(message);
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, message);
 		}
+		return message;
+	}
+
+	@PostMapping("/reset/{email}")
+	@ResponseStatus(HttpStatus.ACCEPTED)
+	public String resetPasswrd(@PathVariable String email) {
+		logger.info("Resetting password for user " + email);
+		statsDClient.incrementCounter("reset.password");
+		String message = userService.resetPassword(email);
 		return message;
 	}
 }
