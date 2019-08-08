317f22be464811c12e1e50f9253915502a70673d
bhavyaharidas <bhavya.haridas93@gmail.com>
2019-08-08 00:41:40 -0400

    Added asg and elb templates

--- infrastructure/aws/cloudformation/AutoScaling/csye6225-aws-cf-create-auto-scaling-application-stack.sh
+++ infrastructure/aws/cloudformation/AutoScaling/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -1,11 +1,11 @@
-echo "Input the new application stack name"
+echo "Input the Auto-Scaling stack name"
 read name
-
-echo "Input ssh key name"
-read keyName
 
 echo "Input network stack name"
 read stackname
+
+echo "Enter your keypair"
+read keyName
 
 echo "Enter your CodeDeploy bucket name "
 read BUCKET_NAME
@@ -17,34 +17,46 @@ echo "Enter your Lambda Revision bucket name "
 read LAMBDA_BUCKET_NAME
 
 echo "Enter your domain name "
-read DOMAIN_NAME
-
-echo "Enter your AWS Acc ID "
-read AccId
+read DomainName
 
 amitag='custom-centos'
 vpcname=$stackname'-csye6225-vpc'
 websubnetname=$stackname'-csye6225-subnet1'
 dbsubnetname=$stackname'-csye6225-subnet3'
-groupname='csye6225-webapps-sg'
 rdsgroupname='csye6225-rds-sg'
+
+
+amiID=$(aws ec2 describe-images --owners self --query 'sort_by(Images, &CreationDate)[-1].ImageId' --output text)
+echo "AMI ID:${amiID}"
+
+hostedzoneid=$(aws route53 list-hosted-zones --query HostedZones[0].Id --output=text | awk -F '/' '{ print $3 }')
+echo "Hosted zone id: $hostedzoneid"
+
+hostedzonename=$(aws route53 list-hosted-zones --query "HostedZones[0].Name" --output text)
+echo "Hosted zone name: $hostedzonename"
+
+
+AccId=$(aws sts get-caller-identity --output text --query 'Account')
+echo "AWS AccountId: $AccId"
 
 amiID=`aws ec2 describe-images --filters "Name=tag:custom,Values=custom-centos" --query 'Images[0].ImageId' --output text`
 #amiID='aws ec2 describe-images --filters "Name=tag:custom,Values=custom-centos" --query 'Images[0].ImageId' --output text'
 VPCID=`aws ec2 describe-tags --filters "Name=value,Values=$vpcname" --query 'Tags[0].ResourceId' --output text`
 webAppSubnetID=`aws ec2 describe-tags --filters "Name=value,Values=$websubnetname" --query 'Tags[0].ResourceId' --output text`
 dbSubnetID=`aws ec2 describe-tags --filters "Name=value,Values=$dbsubnetname" --query 'Tags[0].ResourceId' --output text`
-GROUPID=`aws ec2 describe-tags --filters "Name=value,Values=$groupname" --query 'Tags[0].ResourceId' --output text`
 #RDSGROUPID=`aws ec2 describe-tags --filters "Name=value,Values=$rdsgroupname" --query 'Tags[0].ResourceId' --output text`
 InstanceType='t2.micro'
+WEB_ACL_ID='d9e81b85-7586-439b-98a1-88b3dcb32d38'
 
-stackId=$(aws cloudformation create-stack --stack-name $name --capabilities CAPABILITY_NAMED_IAM --template-body file://csye6225-cf-application.json --parameters "ParameterKey=stackName,ParameterValue=$name" \
+stackId=$(aws cloudformation create-stack --stack-name $name --capabilities CAPABILITY_NAMED_IAM --template-body file://csye6225-cf-auto-scaling-application.json --parameters "ParameterKey=stackName,ParameterValue=$name" \
 "ParameterKey=KeyName,ParameterValue=$keyName" "ParameterKey=ImageId,ParameterValue=$amiID" \
 "ParameterKey=InstanceType,ParameterValue=$InstanceType" "ParameterKey=VPCID,ParameterValue=$VPCID" \
 "ParameterKey=webAppSubnetID,ParameterValue=$webAppSubnetID" "ParameterKey=dbSubnetID,ParameterValue=$dbSubnetID" \
-"ParameterKey=SecurityGroupID,ParameterValue=$GROUPID" "ParameterKey=BucketName,ParameterValue=$BUCKET_NAME" \
+"ParameterKey=BucketName,ParameterValue=$BUCKET_NAME" \
 "ParameterKey=AccId,ParameterValue=$AccId" "ParameterKey=Img,ParameterValue=$IMG_BUCKET_NAME" \
-"ParameterKey=LambdaBucketName,ParameterValue=$LAMBDA_BUCKET_NAME" "ParameterKey=DomainName,ParameterValue=$DOMAIN_NAME" \
+"ParameterKey=LambdaBucketName,ParameterValue=$LAMBDA_BUCKET_NAME" "ParameterKey=DomainName,ParameterValue=$DomainName" \
+"ParameterKey=HostedZoneID,ParameterValue=$hostedzoneid" "ParameterKey=hostedzonename,ParameterValue=$hostedzonename" \
+"ParameterKey=WebACLId,ParameterValue=$WEB_ACL_ID" \
 --query [StackId] --output text)
 
 echo "Stack Id - "
